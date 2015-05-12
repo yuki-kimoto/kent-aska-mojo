004ae3c274f2d5961447a97296632ee16e555ac4
Yuki Kimoto <kimoto.yuki@gmail.com>
2015-05-12 16:49:13 +0900

    セットアップスクリプトを修正

--- setup.sh
+++ setup.sh
@@ -2,4 +2,3 @@
 perl cpanm -n -l extlib Module::CoreList
 perl cpanm -f -l extlib Mojolicious@6.10
 perl -Iextlib/lib/perl5 cpanm -n -L extlib --installdeps .
-perl cpanm -n -l extlib GD::SecurityImage@1.70
