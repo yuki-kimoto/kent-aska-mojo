8f8dc9bc8e6bf06468fe36bd88b20f7d8195e53e
Yuki Kimoto <kimoto.yuki@gmail.com>
2015-05-15 10:35:03 +0900

    cleanup

--- aska.pl
+++ aska.pl
@@ -312,24 +312,4 @@ EOM
   $ml->send();
 });
 
-#  エラー処理
-app->helper('aska.err_img' => sub {
-  my $self = shift;
-  
-  # エラー画像
-  my @err = qw{
-    47 49 46 38 39 61 2d 00 0f 00 80 00 00 00 00 00 ff ff ff 2c
-    00 00 00 00 2d 00 0f 00 00 02 49 8c 8f a9 cb ed 0f a3 9c 34
-    81 7b 03 ce 7a 23 7c 6c 00 c4 19 5c 76 8e dd ca 96 8c 9b b6
-    63 89 aa ee 22 ca 3a 3d db 6a 03 f3 74 40 ac 55 ee 11 dc f9
-    42 bd 22 f0 a7 34 2d 63 4e 9c 87 c7 93 fe b2 95 ae f7 0b 0e
-    8b c7 de 02 00 3b
-  };
-
-  print "Content-type: image/gif\n\n";
-  foreach (@err) {
-    print pack('C*', hex($_));
-  }
-});
-
 app->start;
